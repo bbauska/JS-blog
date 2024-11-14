909993ad874c0ed1cb3e27602507d4cea6333821
Brian Bauska <brianbauska@gmail.com>
2024-11-13 21:08:26 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+const blogPosts = [
+  {
+    title: "First Blog Post",
+    date: "November 13, 2024",
+    content: "This is the content of my first blog post. Excited to share my journey!"
+  },
+  {
+    title: "Learning JavaScript",
+    date: "November 14, 2024",
+    content: "JavaScript is an amazing language for web development. I'm enjoying it!"
+  }
+];
+
+const blogContainer = document.getElementById("blog-posts");
+
+blogPosts.forEach(post => {
+  const article = document.createElement("article");
+  article.innerHTML = `
+    <h2>${post.title}</h2>
+    <p><small>${post.date}</small></p>
+    <p>${post.content}</p>
+    <hr>
+  `;
+  blogContainer.appendChild(article);
+});
 
